ec5fdba941fd347f4280ff17f8d4a07824b9d7b0
anxos <ailampon@gmail.com>
2015-10-08 14:32:24 +0200

    add custom css

--- jumble.js
+++ jumble.js
@@ -2,31 +2,52 @@
 $(document).ready(function(){
     /* lang functionality */
     var CONSTANTS = {
-        'glTitle':'Pódesme ler?',
-'enTitle':'Can you read me?',
+        'glMainTitle':'Pódesme ler?',
+'enMainTitle':'Can you read me?',
+'glTitle':'Pdóesme ler?',
+'enTitle':'Can you raed me?',
 'glIntro':'Dacordo cun investigador da Universidade De Cambridge, non importa a orde das letras nunha palabra. O único importante é que a primeira e a derradeira letra estean no sitio correcto.',
 'enIntro':"According to a researcher at Cambridge University, it doesn't matter in what order the letters in a word are, the only important thing is that the first and last letter be at the right place.",
 'glTextHelper':'Escribe o teu texto aquí',
 'enTextHelper':'Write your text here',
 'glButton':'embarullar!',
-'enButton':'jumble!'
+'enButton':'jumble!',
+'glTextarea':'Docardo cun ivnetsidagor da Udiianvdsere De Cagrmidbe, non irpmtoa a odre das letras nhnua plraaba. O único irtamponte é que a primreia e a dirrredaea letra easten no sitio corrceto',
+'enTextarea':"Accondrig to a rsceeraher at Cmabrdige Uviintrsey , it dsneo't mtater in what oderr the lreetts in a word are, the only inpmtoart tnhig is that the first and lsat ltteer be at the right plcae",
+'glFirefox':'Eu recomendo ',
+'enFirefox':'I recommend ',
+'glImageCredit':'Imaxe de Fondo ',
+'enImageCredit':'Background image ',
+'glTwitter':'Feito por ',
+'enTwitter':'Made by '
     };
 
     /* change to galician */
     $('#gl-btn').click(function(){
         $('html').attr('lang','gl');
+        $('title').text(CONSTANTS.glMainTitle);
         $('h1').text(CONSTANTS.glTitle);
         $('#quote').text(CONSTANTS.glIntro);
         $('#text-helper').text(CONSTANTS.glTextHelper);
         $('#submit-btn').text(CONSTANTS.glButton);
+        $('textarea').val(CONSTANTS.glTextarea);
+        $('span#firefox').text(CONSTANTS.glFirefox);
+        $('span#image-credit').text(CONSTANTS.glImageCredit);
+        $('span#twitter-handle').text(CONSTANTS.glTwitter);
     });
     /* change to english */
     $('#en-btn').click(function(){
         $('html').attr('lang','en');
+        $('title').text(CONSTANTS.enMainTitle);
         $('h1').text(CONSTANTS.enTitle);
         $('#quote').text(CONSTANTS.enIntro);
         $('#text-helper').text(CONSTANTS.enTextHelper);
         $('#submit-btn').text(CONSTANTS.enButton);
+        $('textarea').val(CONSTANTS.enTextarea); 
+        $('span#firefox').text(CONSTANTS.enFirefox);
+        $('span#image-credit').text(CONSTANTS.enImageCredit);
+        $('span#twitter-handle').text(CONSTANTS.enTwitter);
+
     });
 
     /* jumble text functionality */
@@ -59,6 +80,13 @@ $(document).ready(function(){
         return(wordArray.join(''));
     };
 
+
+    /* clean textarea */
+    $('textarea').click(function(){
+        $('textarea').val('');   
+    });
+
+    /* submit text */
     $('#submit-btn').click(function(){
         //get text
         var text = $('textarea').val();
